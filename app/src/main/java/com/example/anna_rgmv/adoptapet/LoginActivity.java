d05e3a3d032a155082c1acb77e05344cbfd16ff2
anna_rgmv <annaragimov@gmail.com>
2017-08-12 03:27:05 +0300

    Contact Us activity page

--- app/src/main/java/com/example/anna_rgmv/adoptapet/LoginActivity.java
+++ app/src/main/java/com/example/anna_rgmv/adoptapet/LoginActivity.java
@@ -66,6 +66,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Add intent
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
